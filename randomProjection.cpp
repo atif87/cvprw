9e662255706f39b0102444a03a91b95c45d4abc7
Atif Iqbal <atif.iiit@gmail.com>
2012-03-08 18:58:58 +0530

    Random Projection

--- randomProjection.cpp
+++ randomProjection.cpp
@@ -10,9 +10,13 @@ int main()
 	float f;
 	int i=0;
 	srand(time(NULL));
-	for(i=0;i<100;i++)
+	for(int j=0;j<1500;j++)
 	{
-		float j =  ((int)(rand())%100)*1.0/50-1 ;
-		cout<<j<<endl;
+		for(i=0;i<1010;i++)
+		{
+			float num =  ((int)(rand())%100)*1.0/50-1 ;
+			cout<<num<<" ";
+		}
+		cout<<endl;
 	}
 }

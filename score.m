9b8cfbf6cc34734507ceeebecec629c6d4bdeb0a
Atif Iqbal <atif.iiit@gmail.com>
2012-03-10 00:04:52 +0530

    score of train vs test + folder reassigned

--- score.m
+++ score.m
@@ -4,7 +4,7 @@ for testuser=1:100
 	for testiter=7:8
 		for trainuser=1:100
 			for trainiter=1:6
-				command = horzcat('bozorth3 -m1 -p minutia/',int2str(testuser),'_',int2str(testiter),'.xyt ','minutia/',int2str(trainuser),'_',int2str(trainiter),'.xyt >> score.txt');
+				command = horzcat('bozorth3 -m1 -p minutia/',int2str(testuser),'_',int2str(testiter),'.xyt ','minutia/',int2str(trainuser),'_',int2str(trainiter),'.xyt >> dataset/score.txt');
 				system(command);
 			end
 		end
